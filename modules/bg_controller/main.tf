abbc5d56295675692e3e975d4244680619f78474
Eli-Meitner <85237294+Eli-Meitner@users.noreply.github.com>
2025-06-18 09:42:35 +0300

    Update main.tf

--- modules/bg_controller/main.tf
+++ modules/bg_controller/main.tf
@@ -89,7 +89,7 @@ resource "aws_codebuild_project" "pr_codebuild" {
 
   environment {
     compute_type                = "BUILD_GENERAL1_SMALL"
-    image                       = "aws/codebuild/amazonlinux2-x86_64-standard:3.0"
+    image                       = "aws/codebuild/amazonlinux2-x86_64-standard:5.0"
     type                        = "LINUX_CONTAINER"
     image_pull_credentials_type = "CODEBUILD"
     privileged_mode             = true
@@ -138,7 +138,7 @@ resource "aws_codebuild_project" "merge_codebuild" {
 
   environment {
     compute_type                = "BUILD_GENERAL1_SMALL"
-    image                       = "aws/codebuild/amazonlinux2-x86_64-standard:3.0"
+    image                       = "aws/codebuild/amazonlinux2-x86_64-standard:5.0"
     type                        = "LINUX_CONTAINER"
     image_pull_credentials_type = "CODEBUILD"
     privileged_mode             = true
